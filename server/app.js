8f3b1cd8b0556632f4958c2fe931644a4510a4d6
SaiAkshitha8010 <saiakshitha8010@gmail.com>
2025-10-31 12:10:53 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'stock-wise-blond.vercel.app'],
+  origin: ['http://localhost:5173', 'https://stock-wise-blond.vercel.app'],
   credentials: true
 }));
 app.use(express.json())
@@ -33,4 +33,5 @@ app.listen(PORT, async() => {
 
 
 export default app;
+
 
